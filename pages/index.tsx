d023b1d55450da345309c17ab9528e42d7a9cd3d
Dean Phommahaxay <deanp21@gmail.com>
2022-12-15 22:03:17 +1300

    Fade slide in home page on initial land

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,29 @@
-import { Container } from '@chakra-ui/react';
+import { Container, keyframes } from '@chakra-ui/react';
 import { CallToAction, Experiences, Hero, Passions } from '../components/home';
 import { PageLayout } from '../components/common';
+
+const fadeSlideIn = keyframes`
+  0% {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+  
+  50% {
+    opacity: 1;
+    transform: translateY(-2px);
+  }
+  
+  100% {
+    transform: translateY(0);
+  }
+`;
+
+const animation = `${fadeSlideIn} 600ms ease-out`;
 
 export default function Home() {
   return (
     <PageLayout pageName="Home">
-      <Container>
+      <Container animation={animation}>
         <Hero />
         <Experiences />
         <Passions />
