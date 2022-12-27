57e800d0e7ad527118b6060d9619faedca909ffc
dphogit <deanp21@gmail.com>
2022-12-28 12:11:21 +1300

    Add vercel audiences for metrics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,17 @@
 import '@fontsource/inter';
 import '@fontsource/ubuntu';
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 import { ChakraProvider } from '@chakra-ui/provider';
 import theme from '../theme';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+      <Analytics />
+    </>
   );
 }
