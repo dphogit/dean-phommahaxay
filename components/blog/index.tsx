3df0c87a19a1fed65c196d463c3e4cae0b4e4bc8
dphogit <deanp21@gmail.com>
2023-02-07 14:36:19 +1300

    task: remove all blog related components

--- components/blog/index.tsx
+++
@@ -1,2 +0,0 @@
-export * from './latest-posts';
-export * from './post';
--- components/blog/post/index.tsx
+++
@@ -1,3 +0,0 @@
-export { default as PostPreview } from './PostPreview';
-export { default as PostHeader } from './PostHeader';
-export { default as PostBody } from './PostBody';
