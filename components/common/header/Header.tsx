9745a3d4c8d0088dff1175e3d65b1b9cb80ec88b
dphogit <deanp21@gmail.com>
2023-02-07 14:47:30 +1300

    style: keep mobile and header nav bar consistent due to now having no other pages to link to

--- components/common/header/Header.tsx
+++ components/common/header/Header.tsx
@@ -1,35 +1,11 @@
-import { Box, Container, Flex, IconButton, Link } from '@chakra-ui/react';
+import { Box, Container, Flex, Link } from '@chakra-ui/react';
 import NextLink from 'next/link';
 import { ResumeButton } from '../resume-button';
 import { ContactButton } from '../contact-button';
 import { Logo } from '../logo';
 import { PageRoutes } from '../../../common/routes';
-import { Cross2Icon, HamburgerMenuIcon } from '@radix-ui/react-icons';
-import { useSSRMediaQuery } from '../../../hooks';
-import { breakpoints } from '../../../theme';
-import { MobileMenu } from './mobile-menu';
-import { useMobileMenu } from './hooks';
-import type { UseMobileMenuItem } from './hooks';
-
-const MENU_ICON_LENGTH = 20;
-
-const MOBILE_MENU_ITEMS: UseMobileMenuItem[] = [
-  {
-    text: 'Home',
-    href: PageRoutes.HOME,
-  },
-  {
-    text: 'Blog',
-    href: PageRoutes.BLOG,
-  },
-];
 
 const Header = () => {
-  const query = `(min-width: ${breakpoints.sm})`;
-  const [isLargerThanSM] = useSSRMediaQuery(query);
-
-  const { isOpen, onToggle, menuItems } = useMobileMenu(MOBILE_MENU_ITEMS);
-
   return (
     <Box
       as="nav"
@@ -38,8 +14,7 @@ const Header = () => {
       top={0}
       bg="white"
       zIndex="sticky"
-      boxShadow={isOpen ? 'none' : 'sm'}
-      height={isOpen ? '100%' : 'auto'}
+      boxShadow="sm"
     >
       <Flex direction="column" py="16px" height="100%" position="relative">
         <Container
@@ -58,34 +33,10 @@ const Header = () => {
             </Link>
           </Box>
           <Flex columnGap="16px" alignItems="center" justifyContent="flex-end">
-            {isLargerThanSM ? (
-              <>
-                <ResumeButton />
-                <ContactButton />
-              </>
-            ) : (
-              <IconButton
-                aria-label="Open menu"
-                icon={
-                  isOpen ? (
-                    <Cross2Icon
-                      height={MENU_ICON_LENGTH}
-                      width={MENU_ICON_LENGTH}
-                    />
-                  ) : (
-                    <HamburgerMenuIcon
-                      height={MENU_ICON_LENGTH}
-                      width={MENU_ICON_LENGTH}
-                    />
-                  )
-                }
-                variant="ghost"
-                onClick={onToggle}
-              />
-            )}
+            <ResumeButton />
+            <ContactButton />
           </Flex>
         </Container>
-        {isOpen && <MobileMenu menuItems={menuItems} />}
       </Flex>
     </Box>
   );
