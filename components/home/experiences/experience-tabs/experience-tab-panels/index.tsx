57f6d2fa94db530e64291677618730151da716a2
dphogit <deanp21@gmail.com>
2026-04-30 21:35:58 +1200

    Add auror experience

--- components/home/experiences/experience-tabs/experience-tab-panels/index.tsx
+++ components/home/experiences/experience-tabs/experience-tab-panels/index.tsx
@@ -1,7 +1,7 @@
 export { default as ExperienceTabPanel } from './ExperienceTabPanel';
 
 // Compositions of ExperienceTabPanel
-export { default as AWSTabPanel } from './AWSTabPanel';
+export { default as AurorTabPanel } from './AurorTabPanel';
 export { default as DatacomTabPanel } from './DatacomTabPanel';
 export { default as ScottTabPanel } from './ScottTabPanel';
 export { default as SegnaTabPanel } from './SegnaTabPanel';
